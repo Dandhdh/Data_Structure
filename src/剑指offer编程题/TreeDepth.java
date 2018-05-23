17689b75a736d385b28ccfcfb0c6e073f2ad6729
danyy <405507582@qq.com>
2018-05-23 17:07:18 +0800

    树的深度

---
+++ src/剑指offer编程题/TreeDepth.java
@@ -0,0 +1,28 @@
+package 剑指offer编程题;
+
+/**
+ * 输入一棵二叉树，求该树的深度。从根结点到叶结点依次经过的结点（含根、叶结点）形成树的一条路径，
+ * 最长路径的长度为树的深度。
+ *
+ * 思路：利用递归遍历分别返回左右子树深度
+ */
+public class TreeDepth {
+
+    public int TreeDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        int left = TreeDepth(root.left);
+        int right = TreeDepth(root.right);
+        return left > right ? left + 1 : right + 1;
+    }
+
+    static class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        public TreeNode(int val){
+            this.val = val;
+        }
+    }
+}
